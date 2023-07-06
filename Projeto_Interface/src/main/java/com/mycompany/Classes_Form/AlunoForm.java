14d2b9ff83d3000a2ae96cbadca1074865f61fdd
ESTEV40 <sousaestevao17@gmail.com>
2023-07-06 04:38:53 -0300

    Atualizando

--- Projeto_Interface/src/main/java/com/mycompany/Classes_Form/AlunoForm.java
+++ Projeto_Interface/src/main/java/com/mycompany/Classes_Form/AlunoForm.java
@@ -3,6 +3,10 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JInternalFrame.java to edit this template
  */
 package com.mycompany.Classes_Form;
+import com.mycompany.ClassesArquivo.DaoAbstract;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 import javax.swing.JOptionPane;
 
 /**
@@ -26,6 +30,8 @@ public class AlunoForm extends javax.swing.JInternalFrame {
         jButtonTrancar = new javax.swing.JButton();
         jButtonVerNota = new javax.swing.JButton();
         jButtonLimpar = new javax.swing.JButton();
+        TextFieldMatricula = new javax.swing.JTextField();
+        jLabel1 = new javax.swing.JLabel();
 
         setClosable(true);
         setMaximizable(true);
@@ -63,28 +69,30 @@ public class AlunoForm extends javax.swing.JInternalFrame {
             }
         });
 
+        jLabel1.setText("Matricula:");
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addContainerGap(60, Short.MAX_VALUE)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(layout.createSequentialGroup()
-                        .addGap(40, 40, 40)
-                        .addComponent(img, javax.swing.GroupLayout.PREFERRED_SIZE, 195, javax.swing.GroupLayout.PREFERRED_SIZE))
-                    .addGroup(layout.createSequentialGroup()
-                        .addGap(17, 17, 17)
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(IdDisc)
-                            .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
-                                .addGroup(layout.createSequentialGroup()
-                                    .addComponent(jButtonTrancar)
-                                    .addGap(18, 18, 18)
-                                    .addComponent(jButtonVerNota)
-                                    .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                                    .addComponent(jButtonLimpar))
-                                .addComponent(TextFieldIDDisc, javax.swing.GroupLayout.PREFERRED_SIZE, 293, javax.swing.GroupLayout.PREFERRED_SIZE)))))
-                .addContainerGap(25, Short.MAX_VALUE))
+                    .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                        .addGroup(layout.createSequentialGroup()
+                            .addComponent(jButtonTrancar)
+                            .addGap(18, 18, 18)
+                            .addComponent(jButtonVerNota)
+                            .addGap(49, 49, 49)
+                            .addComponent(jButtonLimpar))
+                        .addGroup(layout.createSequentialGroup()
+                            .addGap(23, 23, 23)
+                            .addComponent(img, javax.swing.GroupLayout.PREFERRED_SIZE, 195, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addComponent(IdDisc)
+                        .addComponent(TextFieldIDDisc)
+                        .addComponent(TextFieldMatricula))
+                    .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 88, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(53, 53, 53))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -95,12 +103,16 @@ public class AlunoForm extends javax.swing.JInternalFrame {
                 .addComponent(IdDisc)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(TextFieldIDDisc, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 33, Short.MAX_VALUE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jLabel1)
+                .addGap(5, 5, 5)
+                .addComponent(TextFieldMatricula, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 43, Short.MAX_VALUE)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jButtonTrancar)
                     .addComponent(jButtonVerNota)
                     .addComponent(jButtonLimpar))
-                .addGap(30, 30, 30))
+                .addGap(31, 31, 31))
         );
 
         pack();
@@ -111,12 +123,52 @@ public class AlunoForm extends javax.swing.JInternalFrame {
     }//GEN-LAST:event_TextFieldIDDiscActionPerformed
 
     private void jButtonTrancarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButtonTrancarActionPerformed
+        
+        String IDDisciplina = TextFieldIDDisc.getText();
+        String Matricula = TextFieldMatricula.getText();
+        
+        File file = new File(IDDisciplina + ".csv");
+        
+        if(!file.exists()){
+            JOptionPane.showMessageDialog(null, "Disciplina inexistente");
+        }else{
+            
+            DaoAbstract dao = new DaoAbstract();
+            dao.DeleteArchive(IDDisciplina+".csv", Matricula);
+            JOptionPane.showMessageDialog(null, "Disciplina Trancada com sucesso");
+            
+        }
+        dispose();
+        
         JOptionPane.showMessageDialog(null, "Disciplina Trancada com sucesso");
         dispose();
     }//GEN-LAST:event_jButtonTrancarActionPerformed
 
     private void jButtonVerNotaActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButtonVerNotaActionPerformed
-        JOptionPane.showMessageDialog(null, "Sua nota é: ");
+        
+        String IDDisciplina = TextFieldIDDisc.getText();
+        String Matricula = TextFieldMatricula.getText();
+        
+        File file = new File(IDDisciplina + ".csv");
+        
+        if(!file.exists()){
+            JOptionPane.showMessageDialog(null, "Disciplina inexistente");
+        }else{
+            
+            List<String> LinhasArquivos = new ArrayList<>();
+            DaoAbstract dao = new DaoAbstract();
+            
+            LinhasArquivos = dao.ReadArchive(IDDisciplina + ".csv");
+            
+            for(int i = 1; i < LinhasArquivos.size(); i++){
+                String []Compare = LinhasArquivos.get(i).split(";");
+                if(Compare[0].equals(Matricula)){
+                    float Nota = (Float.parseFloat(Compare[1]) + Float.parseFloat(Compare[2]) + Float.parseFloat(Compare[3]))/3;
+                    JOptionPane.showMessageDialog(null, "Sua nota é: " + Nota);
+                }
+            }
+                        
+        }
         dispose();
     }//GEN-LAST:event_jButtonVerNotaActionPerformed
 
@@ -128,9 +180,11 @@ public class AlunoForm extends javax.swing.JInternalFrame {
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JLabel IdDisc;
     private javax.swing.JTextField TextFieldIDDisc;
+    private javax.swing.JTextField TextFieldMatricula;
     private javax.swing.JLabel img;
     private javax.swing.JButton jButtonLimpar;
     private javax.swing.JButton jButtonTrancar;
     private javax.swing.JButton jButtonVerNota;
+    private javax.swing.JLabel jLabel1;
     // End of variables declaration//GEN-END:variables
 }
